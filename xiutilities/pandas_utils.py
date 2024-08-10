8eea9f1eff59e6fbee9df9515e7a7b3f5eda4f7f
Falk B. Schimweg <git@falk.schimweg.de>
2024-08-10 19:57:22 +0200

    Docu

--- xiutilities/pandas_utils.py
+++ xiutilities/pandas_utils.py
@@ -4,6 +4,16 @@ from math import ceil
 
 
 def async_apply(df, f, target_size=10_000, max_cpu=1000, **kwargs):
+    """
+    Use mutltiprocessing to apply ``f()`` to DataFrame ``df``.
+
+    :param df: Input DataFrame
+    :param f: Function to use in the ``apply()``
+    :param target_size: Target size for slices handled in parallel
+    :param max_cpu: Maximum number of cores to use
+    :param kwargs: Keyword arguments forwarded to the ``df.apply()``
+    :return: Result of the ``apply()``
+    """
     n_slices_target = ceil(len(df) / target_size)
     n_slices = min([
         mp.cpu_count(),
@@ -25,7 +35,19 @@ def async_apply(df, f, target_size=10_000, max_cpu=1000, **kwargs):
     return pd.concat(results).copy()
 
 
-def swap_columns(df, mask, column_pairs):
+def swap_columns(df, mask, column_pairs: list[tuple]) -> pd.DataFrame:
+    """
+    Swap column pairs where ``mask`` is ``True``.
+
+    :param df: Input DataFrame
+    :type df: DataFrame
+    :param mask: Mask where to swap the columns
+    :type mask: Series|ndarray|list
+    :param column_pairs: List of column pairs
+    :type column_pairs: list[tuple]
+    :return: Input DataFrame with swapped columns
+    :rtype: DataFrame
+    """
     for c1, c2 in column_pairs:
         df_swap = df.rename({
             c1: c2,
